e9050f967d0bb23e99d23b1f71b5153b76077b26
mativ <selandermathias00@gmail.com>
2022-09-01 18:13:35 +0200

    din mor

--- main.cpp
+++ main.cpp
@@ -20,26 +20,27 @@
 
 using json = nlohmann::json;
 
-std::vector<std::array<int, 2>> Funktion1(int mouseX, int mouseY, std::vector<std::array<int, 2>> posList, int windowWidth, int windowHeight);
-void DrawLines(SDL_Renderer *renderer, std::vector<std::array<int, 2>> posList, int mouseX, int  mouseY, int windowWidth, int windowHeight);
-void SaveToJSON(std::vector<std::array<int, 2>> posList);
+void Funktion1(int mouseX, int mouseY, int windowWidth, int windowHeight);
+void DrawLines(SDL_Renderer *renderer, int mouseX, int  mouseY, int windowWidth, int windowHeight);
+void SaveToJSON();
 std::ifstream LoadJSON();
-std::vector<std::array<int, 2>> Creating(SDL_Renderer *renderer, std::vector<std::array<int, 2>> posList, int windowWidth, int windowHeight, bool *running, SDL_Event ev, int *whichUI);
-void Menu(SDL_Renderer *renderer, int windowWidth, int windowHeight, bool *running, SDL_Event ev, int *whichUI, std::vector<std::array<int, 2>> posList);
+void Creating(SDL_Renderer *renderer, int windowWidth, int windowHeight, bool *running, SDL_Event ev, int *whichUI);
+void Menu(SDL_Renderer *renderer, int windowWidth, int windowHeight, bool *running, SDL_Event ev, int *whichUI);
 void DrawMenuButtons(SDL_Renderer *renderer, std::vector<SDL_Rect> buttons, std::vector<std::string> buttonTitles, int mouseX, int mouseY);
 bool GetMouseHover(SDL_Rect button, int mouseX, int mouseY);
 std::vector<SDL_Rect> SetButtonCoords(int windowWidth, int windowHeight, int buttonWidth, int buttonHeight, int offset, std::vector<SDL_Rect> buttons);
-std::vector<std::array<int, 2>> ViewMenu(SDL_Renderer *renderer, int windowWidth, int windowHeight, bool *running, SDL_Event ev, int *whichUI, int nextWindow);
-void View(SDL_Renderer *renderer, std::vector<std::array<int, 2>> posList, SDL_Event ev, bool *running, int *whichUI);
-std::vector<std::array<int, 2>> AddFinalPoint(int mouseX, int mouseY, std::vector<std::array<int, 2>> posList, int windowWidth, int windowHeight);
-void Play(SDL_Renderer *renderer, std::vector<Obel> obler, bool *running, int *whichUI, SDL_Event ev, int windowWidth, int windowHeight, float deltaTime, Image obelBilled, std::vector<std::array<int, 2>> posList);
+void ViewMenu(SDL_Renderer *renderer, int windowWidth, int windowHeight, bool *running, SDL_Event ev, int *whichUI, int nextWindow);
+void View(SDL_Renderer *renderer, SDL_Event ev, bool *running, int *whichUI);
+void AddFinalPoint(int mouseX, int mouseY, int windowWidth, int windowHeight);
+void Play(SDL_Renderer *renderer, bool *running, int *whichUI, SDL_Event ev, int windowWidth, int windowHeight, float deltaTime, Image obelBilled);
 std::vector<SDL_Rect> DrawTowerMenu(int mouseX, int mouseY, SDL_Renderer *renderer, int windowWidth, int windowHeight);
 std::vector<SDL_Rect> DrawGrid(SDL_Renderer *renderer, int width, int height, int mouseX, int mouseY, int rows, int collums, int buttons, int offset, int startX, int startY, std::vector<std::string> buttonTitles);
 
 namespace obj
 {
     std::vector<Obel> obler { };
-    std::vector<ObelTower>
+    std::vector<ObelTower> towers;
+    std::vector<std::array<int, 2>> posList;
 }
 
 int main(int argc, char *argv[])
@@ -65,10 +66,6 @@ int main(int argc, char *argv[])
     obelBilled.init(renderer, "obelLectio.jpg");
     obelBilled.resizeKA('w', 50, windowHeight);
 
-
-    std::vector<std::array<int, 2>> posList;
-
-
     SDL_Event ev;
     bool running { true };
 
@@ -84,25 +81,25 @@ int main(int argc, char *argv[])
         switch(whichUI)
         {
             case 0:
-                Menu(renderer, windowWidth, windowHeight, &running, ev, &whichUI, posList);
+                Menu(renderer, windowWidth, windowHeight, &running, ev, &whichUI);
                 break;
             case 1:
-                posList = Creating(renderer, posList, windowWidth, windowHeight, &running, ev, &whichUI);
+                Creating(renderer, windowWidth, windowHeight, &running, ev, &whichUI);
                 break;
             case 2:
-                posList = ViewMenu(renderer, windowWidth, windowHeight, &running, ev, &whichUI, 3);
+                ViewMenu(renderer, windowWidth, windowHeight, &running, ev, &whichUI, 3);
                 break;
             case 3:
-                View(renderer, posList, ev, &running, &whichUI);
+                View(renderer, ev, &running, &whichUI);
                 break;
             case 4:
-                posList = ViewMenu(renderer, windowWidth, windowHeight, &running, ev, &whichUI, 5);
+                ViewMenu(renderer, windowWidth, windowHeight, &running, ev, &whichUI, 5);
                 break;
             case 5:
-                obler = Play(renderer, obler, &running, &whichUI, ev, windowWidth, windowHeight, deltaTime, obelBilled, posList);
+                Play(renderer, &running, &whichUI, ev, windowWidth, windowHeight, deltaTime, obelBilled);
                 break;
             default:
-                Menu(renderer, windowWidth, windowHeight, &running, ev, &whichUI, posList);
+                Menu(renderer, windowWidth, windowHeight, &running, ev, &whichUI);
                 break;
         }
     }
@@ -122,56 +119,54 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-std::vector<std::array<int, 2>> Funktion1(int mouseX, int mouseY, std::vector<std::array<int, 2>> posList, int windowWidth, int windowHeight)
+void Funktion1(int mouseX, int mouseY, int windowWidth, int windowHeight)
 {
-    if(posList.size() == 0 && mouseX < windowWidth / 2 && mouseY < windowHeight - (windowHeight / 4) && mouseY > windowHeight / 4)
+    if(obj::posList.size() == 0 && mouseX < windowWidth / 2 && mouseY < windowHeight - (windowHeight / 4) && mouseY > windowHeight / 4)
     {
         std::array<int, 2> temp { 0, mouseY };
-        posList.push_back(temp);
-    }else if(posList.size() == 0 && mouseX > windowWidth / 2 && mouseY < windowHeight - (windowHeight / 4) && mouseY > windowHeight / 4)
+        obj::posList.push_back(temp);
+    }else if(obj::posList.size() == 0 && mouseX > windowWidth / 2 && mouseY < windowHeight - (windowHeight / 4) && mouseY > windowHeight / 4)
     {
         std::array<int, 2> temp { windowWidth, mouseY };
-        posList.push_back(temp);
-    } else if(posList.size() == 0 && mouseY > windowHeight - (windowHeight / 4))
+        obj::posList.push_back(temp);
+    } else if(obj::posList.size() == 0 && mouseY > windowHeight - (windowHeight / 4))
     {
         std::array<int, 2> temp { mouseX, windowHeight};
-        posList.push_back(temp);
-    } else if(posList.size() == 0 && mouseY < windowHeight / 4)
+        obj::posList.push_back(temp);
+    } else if(obj::posList.size() == 0 && mouseY < windowHeight / 4)
     {
         std::array<int, 2> temp { mouseX, 0};
-        posList.push_back(temp);
+        obj::posList.push_back(temp);
     } else
     {
         std::array<int, 2> temp { mouseX, mouseY };
-        posList.push_back(temp);
+        obj::posList.push_back(temp);
     }
-
-    return posList;
 }
 
-void DrawLines(SDL_Renderer *renderer, std::vector<std::array<int, 2>> posList, int mouseX, int  mouseY, int windowWidth, int windowHeight)
+void DrawLines(SDL_Renderer *renderer, int mouseX, int  mouseY, int windowWidth, int windowHeight)
 {
-    if(posList.size() == 0 && mouseX < windowWidth / 2 && mouseY < windowHeight - (windowHeight / 4) && mouseY > windowHeight / 4)
+    if(obj::posList.size() == 0 && mouseX < windowWidth / 2 && mouseY < windowHeight - (windowHeight / 4) && mouseY > windowHeight / 4)
         thickLineRGBA(renderer, 0, mouseY, mouseX, mouseY, 10, 0, 0, 0, 255);
-    else if(posList.size() == 0 && mouseX > windowWidth / 2 && mouseY < windowHeight - (windowHeight / 4) && mouseY > windowHeight / 4)
+    else if(obj::posList.size() == 0 && mouseX > windowWidth / 2 && mouseY < windowHeight - (windowHeight / 4) && mouseY > windowHeight / 4)
         thickLineRGBA(renderer, windowWidth, mouseY, mouseX, mouseY, 10, 0, 0, 0, 255);
-    else if(posList.size() == 0 && mouseY > windowHeight - (windowHeight / 4))
+    else if(obj::posList.size() == 0 && mouseY > windowHeight - (windowHeight / 4))
         thickLineRGBA(renderer, mouseX, windowHeight, mouseX, mouseY, 10, 0, 0, 0, 255);
-    else if(posList.size() == 0 && mouseY < windowHeight / 4)
+    else if(obj::posList.size() == 0 && mouseY < windowHeight / 4)
         thickLineRGBA(renderer, mouseX, 0, mouseX, mouseY, 10, 0, 0, 0, 255);
-    else if(posList.size() >= 2)
+    else if(obj::posList.size() >= 2)
     {
-        for(int i { }; i<posList.size(); ++i)
+        for(int i { }; i<obj::posList.size(); ++i)
         {
-            if(i < posList.size() - 1)
-                thickLineRGBA(renderer, posList[i][0], posList[i][1], posList[i + 1][0], posList[i + 1][1], 10, 0, 0, 0, 255);
+            if(i < obj::posList.size() - 1)
+                thickLineRGBA(renderer, obj::posList[i][0], obj::posList[i][1], obj::posList[i + 1][0], obj::posList[i + 1][1], 10, 0, 0, 0, 255);
         }
-        thickLineRGBA(renderer, posList.back()[0], posList.back()[1], mouseX, mouseY, 10, 0, 0, 0, 255);
-    } else if(posList.size() > 0)
-        thickLineRGBA(renderer, posList[0][0], posList[0][1], mouseX, mouseY, 10, 0, 0, 0, 255);
+        thickLineRGBA(renderer, obj::posList.back()[0], obj::posList.back()[1], mouseX, mouseY, 10, 0, 0, 0, 255);
+    } else if(obj::posList.size() > 0)
+        thickLineRGBA(renderer, obj::posList[0][0], obj::posList[0][1], mouseX, mouseY, 10, 0, 0, 0, 255);
 }
 
-void SaveToJSON(std::vector<std::array<int, 2>> posList)
+void SaveToJSON()
 {
     json j;
     if(std::filesystem::exists("map.json"))
@@ -182,7 +177,7 @@ void SaveToJSON(std::vector<std::array<int, 2>> posList)
 
     int num { static_cast<int>(j.size()) + 1 };
 
-    json j_vec(posList);
+    json j_vec(obj::posList);
     j[std::to_string(num)] = j_vec;
 
     std::ofstream o("map.json");
@@ -196,7 +191,7 @@ std::ifstream LoadJSON()
     return file;
 }
 
-std::vector<std::array<int, 2>> Creating(SDL_Renderer *renderer, std::vector<std::array<int, 2>> posList, int windowWidth, int windowHeight, bool *running, SDL_Event ev, int *whichUI)
+void Creating(SDL_Renderer *renderer, int windowWidth, int windowHeight, bool *running, SDL_Event ev, int *whichUI)
 {
     int mouseX { };
     int mouseY { };
@@ -211,7 +206,7 @@ std::vector<std::array<int, 2>> Creating(SDL_Renderer *renderer, std::vector<std
             switch(ev.button.button)
             {
                 case SDL_BUTTON_LEFT:
-                    posList = Funktion1(mouseX, mouseY, posList, windowWidth, windowHeight);
+                    Funktion1(mouseX, mouseY, windowWidth, windowHeight);
                     break;
             }
         } else if(ev.type == SDL_KEYDOWN)
@@ -219,16 +214,16 @@ std::vector<std::array<int, 2>> Creating(SDL_Renderer *renderer, std::vector<std
             switch(ev.key.keysym.sym)
             {
                 case SDLK_s:
-                    posList = AddFinalPoint(mouseX, mouseY, posList, windowWidth, windowHeight);
-                    SaveToJSON(posList);
+                    AddFinalPoint(mouseX, mouseY, windowWidth, windowHeight);
+                    SaveToJSON();
                     *whichUI = 0;
                     break;
                 case SDLK_r:
-                    posList.clear();
+                    obj::posList.clear();
                     break;
                 case SDLK_z:
-                    if(posList.size() > 0)
-                        posList.pop_back();
+                    if(obj::posList.size() > 0)
+                        obj::posList.pop_back();
                     break;
                 case SDLK_PLUS:
                     *whichUI += 1;
@@ -246,37 +241,34 @@ std::vector<std::array<int, 2>> Creating(SDL_Renderer *renderer, std::vector<std
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
 
-    DrawLines(renderer, posList, mouseX, mouseY, windowWidth, windowHeight);
+    DrawLines(renderer, mouseX, mouseY, windowWidth, windowHeight);
 
     SDL_RenderPresent(renderer);
 
-    return posList;
 }
 
-std::vector<std::array<int, 2>> AddFinalPoint(int mouseX, int mouseY, std::vector<std::array<int, 2>> posList, int windowWidth, int windowHeight)
+void AddFinalPoint(int mouseX, int mouseY, int windowWidth, int windowHeight)
 {
-    if(posList[0][0] == 0)
+    if(obj::posList[0][0] == 0)
     {
-        std::array<int, 2> temp { windowWidth, posList[posList.size() - 1][1] };
-        posList.push_back(temp);
-    } else if(posList[0][0] == windowWidth)
+        std::array<int, 2> temp { windowWidth, obj::posList[obj::posList.size() - 1][1] };
+        obj::posList.push_back(temp);
+    } else if(obj::posList[0][0] == windowWidth)
     {
-        std::array<int, 2> temp { 0, posList[posList.size() - 1][1] };
-        posList.push_back(temp);
-    } else if(posList[0][1] == 0)
+        std::array<int, 2> temp { 0, obj::posList[obj::posList.size() - 1][1] };
+        obj::posList.push_back(temp);
+    } else if(obj::posList[0][1] == 0)
     {
-        std::array<int, 2> temp { posList[posList.size() - 1][0], windowWidth };
-        posList.push_back(temp);
-    } else if(posList[0][1] == windowWidth)
+        std::array<int, 2> temp { obj::posList[obj::posList.size() - 1][0], windowWidth };
+        obj::posList.push_back(temp);
+    } else if(obj::posList[0][1] == windowWidth)
     {
-        std::array<int, 2> temp { posList[posList.size() - 1][0], 0  };
-        posList.push_back(temp);
+        std::array<int, 2> temp { obj::posList[obj::posList.size() - 1][0], 0  };
+        obj::posList.push_back(temp);
     }
-
-    return posList;
 }
 
-void Menu(SDL_Renderer *renderer, int windowWidth, int windowHeight, bool *running, SDL_Event ev, int *whichUI, std::vector<std::array<int, 2>> posList)
+void Menu(SDL_Renderer *renderer, int windowWidth, int windowHeight, bool *running, SDL_Event ev, int *whichUI)
 {
     int mouseX { };
     int mouseY { };
@@ -331,7 +323,6 @@ void Menu(SDL_Renderer *renderer, int windowWidth, int windowHeight, bool *runni
             }
         }
     }
-
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
@@ -396,7 +387,7 @@ std::vector<SDL_Rect> SetButtonCoords(int windowWidth, int windowHeight, int but
     return buttons;
 }
 
-std::vector<std::array<int, 2>> ViewMenu(SDL_Renderer *renderer, int windowWidth, int windowHeight, bool *running, SDL_Event ev, int *whichUI, int nextWindow)
+void ViewMenu(SDL_Renderer *renderer, int windowWidth, int windowHeight, bool *running, SDL_Event ev, int *whichUI, int nextWindow)
 {
     json j;
     LoadJSON() >> j;
@@ -408,8 +399,6 @@ std::vector<std::array<int, 2>> ViewMenu(SDL_Renderer *renderer, int windowWidth
     std::vector<SDL_Rect> buttons(j.size());
     // int buttonHeight { (windowHeight - offset * static_cast<int>(j.size())) / static_cast<int>(j.size()) };
     // buttons = SetButtonCoords(windowWidth, windowHeight, 350, buttonHeight, offset, buttons);
-
-    std::vector<std::array<int, 2>> posList;
 
     int mouseX { };
     int mouseY { };
@@ -446,7 +435,7 @@ std::vector<std::array<int, 2>> ViewMenu(SDL_Renderer *renderer, int windowWidth
                     {
                         if(GetMouseHover(buttons[i], mouseX, mouseY))
                         {
-                            posList = j[std::to_string(i + 1)];
+                            obj::posList = j[std::to_string(i + 1)];
                             *whichUI = nextWindow;
                         }
                     }
@@ -462,11 +451,9 @@ std::vector<std::array<int, 2>> ViewMenu(SDL_Renderer *renderer, int windowWidth
     buttons = DrawGrid(renderer, windowWidth, windowHeight, mouseX, mouseY, 5, 5, buttons.size(), offset, 0, 0, titles);
 
     SDL_RenderPresent(renderer);
-
-    return posList;
 }
 
-void View(SDL_Renderer *renderer, std::vector<std::array<int, 2>> posList, SDL_Event ev, bool *running, int *whichUI)
+void View(SDL_Renderer *renderer, SDL_Event ev, bool *running, int *whichUI)
 {
     while(SDL_PollEvent(&ev) != 0)
     {
@@ -493,16 +480,16 @@ void View(SDL_Renderer *renderer, std::vector<std::array<int, 2>> posList, SDL_E
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
 
-    for(int i { }; i<posList.size(); ++i)
+    for(int i { }; i<obj::posList.size(); ++i)
     {
-        if(i < posList.size() - 1)
-            thickLineRGBA(renderer, posList[i][0], posList[i][1], posList[i + 1][0], posList[i + 1][1], 10, 0, 0, 0, 255);
+        if(i < obj::posList.size() - 1)
+            thickLineRGBA(renderer, obj::posList[i][0], obj::posList[i][1], obj::posList[i + 1][0], obj::posList[i + 1][1], 10, 0, 0, 0, 255);
     }
 
     SDL_RenderPresent(renderer);
 }
 
-void Play(SDL_Renderer *renderer, std::vector<Obel> obler, bool *running, int *whichUI, SDL_Event ev, int windowWidth, int windowHeight, float deltaTime, Image obelBilled, std::vector<std::array<int, 2>> posList)
+void Play(SDL_Renderer *renderer, bool *running, int *whichUI, SDL_Event ev, int windowWidth, int windowHeight, float deltaTime, Image obelBilled)
 {
     int mouseX { };
     int mouseY { };
@@ -527,16 +514,16 @@ void Play(SDL_Renderer *renderer, std::vector<Obel> obler, bool *running, int *w
             {
                 case SDLK_ESCAPE:
                     *whichUI = 4;
-                    for(int i { }; i<obler.size(); ++i)
-                        obler.clear();
+                    for(int i { }; i<obj::obler.size(); ++i)
+                        obj::obler.clear();
                     break;
                 case SDLK_SPACE:
                     Obel temp;
-                    temp.initObel(posList, renderer);
+                    temp.initObel(obj::posList, renderer);
                     temp.billed = obelBilled;
                     temp.speed = 100;
                     temp.hp = 10;
-                    obler.push_back(temp);
+                    obj::obler.push_back(temp);
                     break;
             }
 
@@ -549,9 +536,6 @@ void Play(SDL_Renderer *renderer, std::vector<Obel> obler, bool *running, int *w
                     {
                         whichBuilding = 0;
                         isBuilding = !isBuilding;
-                    } else
-                    {
-                        isBuilding
                     }
                     break;
             }
@@ -561,23 +545,23 @@ void Play(SDL_Renderer *renderer, std::vector<Obel> obler, bool *running, int *w
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
 
-    for(int i { }; i<posList.size(); ++i)
+    for(int i { }; i<obj::posList.size(); ++i)
     {
-        if(i < posList.size() - 1)
-            thickLineRGBA(renderer, posList[i][0], posList[i][1], posList[i + 1][0], posList[i + 1][1], 10, 0, 0, 0, 255);
+        if(i < obj::posList.size() - 1)
+            thickLineRGBA(renderer, obj::posList[i][0], obj::posList[i][1], obj::posList[i + 1][0], obj::posList[i + 1][1], 10, 0, 0, 0, 255);
     }
 
-    for(int i { static_cast<int>(obler.size()) - 1}; i>=0; --i)
+    for(int i { static_cast<int>(obj::obler.size()) - 1}; i>=0; --i)
     {
-        obler[i].obelMove(deltaTime, posList);
-        obler[i].countDown -= 1 * deltaTime;
+        obj::obler[i].obelMove(deltaTime, obj::posList);
+        obj::obler[i].countDown -= 1 * deltaTime;
 
-        obler[i].drawObel(renderer);
+        obj::obler[i].drawObel(renderer);
 
-        if(obler[i].reachedPoint == posList.size() - 1 || obler[i].hp <= 0)
+        if(obj::obler[i].reachedPoint == obj::posList.size() - 1 || obj::obler[i].hp <= 0)
         {
-            obler.erase(obler.begin() + i);
-            obler[i].cleanUp();
+            obj::obler.erase(obj::obler.begin() + i);
+            obj::obler[i].cleanUp();
         }
     }
 
